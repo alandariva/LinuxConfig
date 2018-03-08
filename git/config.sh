216122178ee65360f7737ad313631e2eeb9058db
Alan Oliveira <alan.oliveira@unicred.com.br>
2018-03-08 10:27:54 -0300

    Adiciona pergunta para qual email utilizar no git

--- git/config.sh
+++ git/config.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+defaultEmail="alan.dariva@gmail.com"
+
+read -p "Utilizar qual e-mail para os commits? ($defaultEmail) " userEmail
+
+if [ "$userEmail" == "" ]; then
+    userEmail="$defaultEmail"
+fi
+
 git config --global user.name "Alan Oliveira"
-git config --global user.email alan.dariva@gmail.com
+git config --global user.email $userEmail
 git config --global push.default simple
+git config --global core.editor "vim"
