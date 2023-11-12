4e608d3be27af16e2139d392ba27fffb2a91ac41
Alan Oliveira <alan.dariva@gmail.com>
2023-11-12 16:08:02 -0300

    Remove old files

--- git/credential-storage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install/compile Gnome Keyring devel
-sudo apt-get install -y libgnome-keyring-dev
-sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
-
-# Setup credential
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
-
--- raspberry/nodejs/install-v10.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -  # Install NodeJS v10
-sudo apt-get install -y nodejs
